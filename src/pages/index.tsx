86aa2b40699e35d22f473ff814fdd3fbe8c810ee
Hunter Wilhelm <71348224+hunterwilhelm@users.noreply.github.com>
2023-10-09 14:37:27 -0600

    xenon not working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useEffect } from "react";
+import Xenon from "xenon-view-sdk";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    Xenon.init("");
+  }, []);
   return (
     <>
       <Head>
@@ -26,7 +31,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              By{" "}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -110,5 +115,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
