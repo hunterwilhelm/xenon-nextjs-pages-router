fb41cba4fbec313c414e9a6c4d40c3e77b440b53
Luke Woydziak <luke.woydziak@gmail.com>
2023-12-12 16:50:38 -0700

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
-import HomePage from "@/ui/HomePage";
-// export default dynamic(() => import("../ui/HomePage"), { ssr: false });
+import dynamic from 'next/dynamic'
+// import HomePage from "@/ui/HomePage";
+
+const HomePage =  dynamic(()=>import('@/ui/HomePage'), {ssr:false});
 
 export default HomePage;
